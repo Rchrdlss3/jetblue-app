b180803fefd7c14dbb87cd1ac2da0706935c4115
Richard Ulysse <Rchrdlss3@gmail.com>
2026-02-10 11:19:54 -0500

    updated json and weather api call to https

--- src/utils/weatherAPICalls.ts
+++ src/utils/weatherAPICalls.ts
@@ -1,6 +1,6 @@
 export const getForecast = async (location:string,days:number) => {
     try {
-        const resp =  fetch(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${location}&days=${days}&aqi=no&alerts=no`)
+        const resp =  fetch(`https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${location}&days=${days}&aqi=no&alerts=no`)
         const data = (await resp).json()
         return data
     } catch (e) {
